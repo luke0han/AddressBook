1016d18b66009a29b586401d53b1fd96ddd9d219
luke0han <lukeohanlon@outlook.com>
2020-12-31 17:10:07 +0000

    added search function and theme change

--- main.js
+++ main.js
@@ -1,87 +1,149 @@
-// Open Add New Contact Modal Form
-$('#add-btn')
-.on('click', () => $('.modal')
-.css('display', 'flex'))
-
-//Close Add New Contact Modal Form
-$('#close')
-.on('click', (i) => $('.modal')
-.css('display', 'none'))
-
 // Contact Class
 class Contact {
-    constructor(first, last, phone, address){
-        this.first = first
-        this.last = last
-        this.phone = phone
-        this.address = address
-    }
+  constructor(first, last, phone, address) {
+    this.first = first;
+    this.last = last;
+    this.phone = phone;
+    this.address = address;
+  }
 }
 
 // User Events
 class UserEvents {
-    static displayContacts() {
-        const $SavedContacts = []
-        const $contacts = $SavedContacts
-
-        $contacts.forEach(contact => UserEvents.addNewContact(contact))
+  // Create New Contact Row
+  static addNewContact(contact) {
+    let themeClass = "#fff";
+    let $bodyBG = $("body").css("background-color");
+    if ($bodyBG === "rgba(0, 0, 0, 0)") {
+      themeClass = "default";
+    } else if ($bodyBG === "rgb(222, 243, 246)") {
+      themeClass = "ocean";
+    } else if ($bodyBG === "rgb(7, 4, 10)") {
+      themeClass = "dark";
+    } else if ($bodyBG === "rgb(157, 206, 149)") {
+      themeClass = "forest";
+    } else if ($bodyBG === "rgb(236, 226, 198)") {
+      themeClass = "desert";
     }
 
-    static addNewContact(contact){
-        const $table = $('.table')
-        const $row = $('<tr>')
+    const $row = $("<tr>").addClass(`${themeClass}`)
 
-        $row.html( `
-        <td>${contact.first}</td>
-        <td>${contact.last}</td>
-        <td>${contact.phone}</td>
-        <td>${contact.address}</td>
+    $row.html(`
+        <td class="info">${contact.first}</td>
+        <td class="info">${contact.last}</td>
+        <td class="info">${contact.phone}</td>
+        <td class="info">${contact.address}</td>
         <td><i class="fas fa-trash-alt delete"></i></td>
         `)
-        $table.append($row)
-    }
+    $(".table").append($row)
+  }
 
-    static clearForm() {
-        $('#first-name').val('')
-        $('#last-name').val('')
-        $('#phone').val('')
-        $('#address').val('')
-    }
-
-    static deleteContact(e){
-        e.closest('tr').remove()
-    }
+  // Clear All Form Fields
+  static clearForm() {
+    $("#first-name").val("")
+    $("#last-name").val("")
+    $("#phone").val("")
+    $("#address").val("")
+  }
 }
 
-// Store Contacts
-
-// Display Contacts
-$(document).ready(UserEvents.displayContacts)
 // Add Contact
-$('.modal-button').on('click', (e) => {
-    // prevent submit default
-    e.preventDefault();
-    // store input values in variables
-    const $first = $('#first-name').val()
-    const $last = $('#last-name').val()
-    const $phone = $('#phone').val()
-    const $address = $('#address').val()
+$(".modal-button").on("click", (e) => {
+  // Prevent Submit Default
+  e.preventDefault()
+  // Store Input Values In Variables
+  const $first = $("#first-name").val()
+  const $last = $("#last-name").val()
+  const $phone = $("#phone").val()
+  const $address = $("#address").val()
+  //Check If Any Fields Are Empty
+  if ($first === "" || $last === "" || $phone === "" || $address === "") {
+    alert("Please Fill In All Fields!");
+  } else {
+    const $contact = new Contact($first, $last, $phone, $address);
+    UserEvents.addNewContact($contact);
+    // Hide Modal
+    $(".modal").css("display", "none");
+    // Clear Form
+    UserEvents.clearForm()
+  }
+})
 
-    if($first === '' || $last === '' || $phone === '' || $address === '') {
-     alert('Please Fill In All Fields!')
-    } else{
-         const $contact =  new Contact($first, $last, $phone, $address)
-      UserEvents.addNewContact($contact)
-      // Hide modal
-      $('.modal').css('display', 'none')
-      // Clear form
-      UserEvents.clearForm()
+// Delete Contact
+$(".table").on("click", ".delete", (e) => {
+  console.log("hi!");
+  e.target.closest("tr").remove()
+})
+
+// Open Add New Contact Modal Form
+$("#add-btn").on("click", () => $(".modal").css("display", "flex"));
+
+//Close Add New Contact Modal Form
+$("#close").on("click", (i) => {
+  $(".modal").css("display", "none");
+  UserEvents.clearForm()
+})
+
+// Open Theme Form
+$(".theme").on("click", () => $(".modal-theme").css("display", "flex"));
+
+// Close Theme Form
+$(".close").on("click", (i) => {
+  $(".modal-theme").css("display", "none");
+})
+
+// Save Theme Choice
+$(".theme-btn").on("click", () => {
+  let colorTheme = "#2e2e2e";
+  let colorBG = "#fff";
+
+  if ($(".modal-body input[id='default']:checked").val()) {
+    colorTheme = "#2e2e2e";
+    colorBG = "#fff";
+    $(".table tbody tr:nth-child(odd)").removeClass().addClass("default");
+  } else if ($(".modal-body input[id='dark']:checked").val()) {
+    colorTheme = "#a73bff";
+    colorBG = "#07040a";
+    $(".table tbody tr").removeClass().addClass("dark");
+  } else if ($(".modal-body input[id='ocean']:checked").val()) {
+    colorTheme = "#064273";
+    colorBG = "#def3f6";
+    $(".table tbody tr").removeClass().addClass("ocean");
+  } else if ($("input[id='forest']:checked").val()) {
+    colorTheme = "#4c4442";
+    colorBG = "#9dce95";
+    $(".table tbody tr").removeClass().addClass("forest");
+  } else if ($("input[id='desert']:checked").val()) {
+    colorTheme = "#6C541E";
+    colorBG = "#ECE2C6";
+    $(".table tbody tr").removeClass().addClass("desert");
+  }
+  $("body").css("color", `${colorTheme}`);
+  $("body").css("background-color", `${colorBG}`);
+  $("button").css("background-color", `${colorTheme}`);
+  $(".modal-button").css("background-color", `${colorTheme}`);
+  $(".table thead tr").css("background-color", `${colorTheme}`);
+  $(".modal-head").css("background-color", `${colorTheme}`);
+  $(".modal-theme").css("display", "none");
+})
+
+// Search Contacts
+$(".search").on("keyup", (e) => {
+  const searchVal = e.target.value.toLowerCase().replace(/\s*/g, "");
+  $(".info").each((i, td) => {
+    const $siblings = $(td).siblings().text().toLowerCase();
+    const $current = $(td).text().toLowerCase();
+    let str = $siblings + $current;
+    if (str.indexOf(searchVal) !== -1) {
+      $(td).closest("tr").show();
+    } else {
+      $(td).closest("tr").hide();
     }
+  })
 })
 
-// Remove Contact
-$('.table').on('click', (e) => {
-    UserEvents.deleteContact(e.target)
+// Clear Search and Show Contacts
+$(".search").focusout(() => {
+    $('.search').val('')
+    $('.info').closest('tr').show()
 })
-
-
